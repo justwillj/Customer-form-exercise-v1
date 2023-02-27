f4d944df72d30e9956482e6886b877f5dbee8440
Justin Williams <williams.jman@gmail.com>
2023-02-26 22:29:01 -0500

    Added the check for the email

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,10 +5,26 @@ import { useState } from "react";
 const App = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [formError, setFormError] = useState(false);
+  const [zipCode, setZipCode] = useState("");
+
+  //Link that helped me with this
+  //https://stackoverflow.com/questions/72092658/how-can-i-validate-react-input-is-not-empty-and-valid-email
+  const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+  //Toggles the errors
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [zipCodeError, setZipCodeError] = useState(false);
+
   const formCheck = (event) => {
+    //Handles the name check
     if (name.length < 3) {
-      setFormError(true);
+      setNameError(true);
+      event.preventDefault();
+    }
+
+    if (!email.match(validEmail && email.length === 0)) {
+      setEmailError(true);
       event.preventDefault();
     }
   };
@@ -16,18 +32,35 @@ const App = () => {
   const inputOnChangeName = (e) => {
     setName(e.target.value);
   };
+
+  const inputOnChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const inputOnChangeZipCode = (e) => {
+    setZipCode(e.target.value);
+  };
+
   return (
     <div>
       <h1>Customer form </h1>
       <form onSubmit={formCheck}>
         <label>Name:</label>
-        {formError ? <p>Must be at least 3 characters</p> : null}
+        {nameError ? <p>Must be at least 3 characters</p> : null}
         <input type="text" value={name} onChange={inputOnChangeName}></input>
+
         <label>Email:</label>
-        <input type="email"></input>
+        {emailError ? <p>Must be a valid email</p> : null}
+        <input type="email" value={email} onChange={inputOnChangeEmail}></input>
 
         <label>Zip Code:</label>
-        <input type="number"></input>
+        {zipCodeError ? <p>Must be a valid zip code</p> : null}
+        <input
+          type="number"
+          value={zipCode}
+          onChange={inputOnChangeZipCode}
+        ></input>
+
         <input type="submit"></input>
       </form>
     </div>
