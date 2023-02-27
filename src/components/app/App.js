1b6660afa51342d47b45bba551015129fc77bac9
Justin Williams <williams.jman@gmail.com>
2023-02-26 22:07:26 -0500

    Created the validation for name

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [formError, setFormError] = useState(false);
+  const formCheck = (event) => {
+    if (name.length < 3) {
+      setFormError(true);
+      event.preventDefault();
+    }
+  };
+
+  const inputOnChangeName = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div>
       <h1>Customer form </h1>
-      <form>
+      <form onSubmit={formCheck}>
         <label>Name:</label>
-        <input type="text"></input>
-
+        {formError ? <p>Must be at least 3 characters</p> : null}
+        <input type="text" value={name} onChange={inputOnChangeName}></input>
         <label>Email:</label>
         <input type="email"></input>
 
