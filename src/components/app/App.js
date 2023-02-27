5a952a49bce80a0f72b7365848ed1a13228b3097
Justin Williams <williams.jman@gmail.com>
2023-02-26 20:28:58 -0500

    Created the layout for the form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,22 @@
+import Input from "../input/Input";
 import "./App.css";
-import "./App.css";
+import { useState } from "react";
+
 const App = () => {
   return (
     <div>
-      <h1>test</h1>
+      <h1>Customer form </h1>
+      <form>
+        <label>Name:</label>
+        <input type="text"></input>
+
+        <label>Email:</label>
+        <input type="email"></input>
+
+        <label>Zip Code:</label>
+        <input type="number"></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
 };
